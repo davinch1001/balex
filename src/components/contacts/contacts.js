6750e037af5de64583fd5abfce82ed92e7eedd5a
Shabdan <shabdan.abjaparov,official@gmail.com>
2021-08-07 19:28:05 +0600

    added new functions

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -8,16 +8,16 @@ const Contacts = () => {
                 <div className="contacts__block">
                     <div className="contacts__social-network">
                         <div className="social-network-title">Следи за нами в instagram</div>
-                        <a href="#" className="social-network-link">@belex</a>
+                        <a href="https://www.instagram.com/balex.kg/" target="_blank" className="social-network-link">@balex.kg</a>
                         <p className="social-network-abouts">В Instagram мы рассказываем, как и где покупать в
                             США, информируем о сезонных и незапланированных распродажах в магазинах, скидываем
-                            выгодные лоты, а также делимся другим полезным контентом.</p>
+                            выгодные лготы, а также делимся другим полезным контентом.</p>
                     </div>
                     <div className="contacts__numbers">
                         <h2 className="numbers-title">Свяжитесь с нами по телефону</h2>
                             <p className="number">+996 (555) 555-555</p>
                             <p className="number">+996 (555) 555-555</p>
-                            <span className="abouts">24/7 тех. поддержка и бла бла бла бал бала б</span>
+                            <span className="abouts">24/7 Служба поддержки</span>
 
                     </div>
                 </div>
